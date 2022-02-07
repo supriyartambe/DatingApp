9d395a082cd474379551270e93f236cc0bb330f3
supriyartambe <97288565+supriyartambe@users.noreply.github.com>
2022-02-07 11:05:49 +0530

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,6 +11,9 @@ namespace API.Entities
 
         public string UserName { get; set; }
 
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
         
     }
 }
