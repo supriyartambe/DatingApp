77f5b93b0ca0443b98e917c41cfcce217e3617d8
supriyartambe <97288565+supriyartambe@users.noreply.github.com>
2022-02-14 15:28:07 +0530

    End of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -23,8 +23,7 @@ export class AccountService {
         const user = response;
         if(user)
         {
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -35,14 +34,14 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/register',model).pipe(
       map((user:User) => {
         if(user){
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+         this.setCurrentUser(user);
         }
       })
     )
   }
 setCurrentUser(user:User)
 {
+  localStorage.setItem('user',JSON.stringify(user));
   this.currentUserSource.next(user);
 }
 
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -43,5 +43,14 @@ updateMember(member:Member)
   );
 }
 
+setMainPhoto(photoId:number)
+{
+  return this.http.put(this.baseUrl + 'users/set-main-photo/' +photoId, {});
+}
+
+    deletePhoto(photoId: number)
+    {
+      return this.http.delete(this.baseUrl + 'users/delete-photo/' +photoId);
+    }
 
 }
